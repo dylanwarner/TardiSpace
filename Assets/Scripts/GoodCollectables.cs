425c51c6229bd3ee6ed8be1509e0dbd0f4084257
Dylan Warner <dcw8084@g.rit.edu>
2019-09-12 10:19:31 -0400

    Uploaded project

---
+++ Assets/Scripts/GoodCollectables.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GoodCollectables : MonoBehaviour {
+
+	public GameObject stardustPrefab;
+	public List<GameObject> stardusts;
+	public int maxStardusts = 10;
+
+	public GameObject starleafPrefab;
+	public List<GameObject> starleaves;
+	public int maxStarleaves = 5;
+
+	 
+	// initialization
+	void Start () 
+	{
+		CreateStardust ();
+		CreateStarLeaves ();
+	}
+
+	void CreateStardust()
+	{
+		stardusts = new List<GameObject> ();
+
+		// Spawning stardusts randomly on the screen within the screen's boundaries
+		// Quaternion.identity - no rotation
+		for (int i = 0; i < maxStardusts; i++) 
+		{
+			Vector3 screenPosition = new Vector3 (Random.Range (-8.29f, 8.29f), 
+				Random.Range (-4.1f, 3.8f), 1);
+			GameObject myStardusts = stardustPrefab;
+			Instantiate (myStardusts, screenPosition, Quaternion.identity);
+			stardusts.Add (myStardusts);
+
+		}
+	}
+		
+	void CreateStarLeaves()
+	{
+		starleaves = new List<GameObject> ();
+
+		// Spawning starleaves randomly on the screen within the screen's boundaries
+		for (int j = 0; j < maxStarleaves; j++) 
+		{
+			Vector3 screenPosition = new Vector3 (Random.Range (-8.29f, 8.29f), 
+				Random.Range (-4.1f, 3.8f), 1);
+			GameObject myStarLeaves = starleafPrefab;
+			Instantiate (myStarLeaves, screenPosition, Quaternion.identity);
+			stardusts.Add (myStarLeaves);
+		}
+	}
+}
+	
---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public void LoadScene(int level) //loadscene takes a parameter of the type int. index of the level of the build setting
+    {
+        Application.LoadLevel(level); //loads scenes in unity. parameter 'level' is the index in the build settings of the level we want loaded
+    }
+
+}
